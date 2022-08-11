d826f5c6cd9c025c178853dd85a8cbb7d88fe7a5
Nafay Rizwani <80161736+Nafay-0@users.noreply.github.com>
2022-08-11 19:57:30 +0500

    Register and profile page added

---
+++ frontEnd/src/Pages/SignUpPage.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+
+
+
+const SignUpPage = () => {
+
+
+    const [FormData, setFormData] = React.useState({
+        email: '',
+        password: '',
+        username: '',
+    });
+    function HandleFormSubmit(e) {
+        e.preventDefault();
+        console.log(FormData);
+    }
+    return (
+        <div>
+            <section className="h-screen">
+                <div className="container px-6 py-12 h-full">
+                    <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+                        <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+                            <img
+                                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                                className="w-full"
+                                alt="Phone image"
+                            />
+                        </div>
+                        <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+                            <form onSubmit={HandleFormSubmit}>
+                                <div className="mb-6">
+                                    <input
+                                        type="name"
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        placeholder="Enter your username"
+                                        value={FormData.name}
+                                        id="username"
+                                        name='username'
+                                        onChange={(e) => setFormData({ ...FormData, username: e.target.value })}
+                                    />
+                                </div>
+                                <div className="mb-6">
+                                    <input
+                                        type="text"
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        placeholder="Email address"
+                                        value={FormData.email}
+                                        id="email"
+                                        name='email'
+                                        onChange={(e) => setFormData({ ...FormData, email: e.target.value })}
+                                    />
+                                </div>
+                                <div className="mb-6">
+                                    <input
+                                        type="password"
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        placeholder="Password"
+                                        value={FormData.password}
+                                        id="password"
+                                        name='password'
+                                        onChange={(e) => setFormData({ ...FormData, password: e.target.value })}
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                                    data-mdb-ripple="true"
+                                    data-mdb-ripple-color="light"
+                                >
+                                    Register
+                                </button>
+                                <div
+                                    className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5"
+                                >
+                                    <p className="text-center font-semibold mx-4 mb-0">OR</p>
+                                </div>
+
+                                <a
+                                    className="px-7 py-3 bg-slate-900 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+
+                                    href="/"
+                                    role="button"
+                                    data-mdb-ripple="true"
+                                    data-mdb-ripple-color="light"
+                                >
+                                    Already have an account? Sign in
+                                </a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
+
+export default SignUpPage;
